bf23d225cf9e42cafc0b5932f153e6152eb05ec3
Maria Tedoradze <mtedoradze@gmail.com>
2022-11-24 15:29:02 +0300

    Add message on telegram to actions

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Теперь за меня все делает скрипт!')
 
 
 def second_page(request):
